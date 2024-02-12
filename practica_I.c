42b384fbf6d890650cf7de499566bfdb09b92b83
Enrique <Enrique.Valverde1@alu.uclm.es>
2024-02-12 20:33:16 +0100

    update all algs

--- practica_I.c
+++ practica_I.c
@@ -1,17 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
+#include <string.h>
 
 // Function to multiply two matrices A and B with loop ordering ijk
 void multiplyMatrices_ijk(int m, int k, int n, double **A, double **B, double **C) {
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
-            C[i][j] = 0.0;
             for (int l = 0; l < k; l++) {
                 C[i][j] += A[i][l] * B[l][j];
             }
         }
     }
+}
+
+// Function to multiply two matrices A and B with loop ordering ijk
+void multiplyMatrices_jki(int m, int k, int n, double **A, double **B, double **C) {
+    for (int j = 0; j < n; j++) {
+        for (int l = 0; l < k; l++) {
+            for (int i = 0; i < m; m++) {
+                C[i][j] += A[i][l] * B[l][j];
+            }
+        }
+    }
+}
+
+// Function to multiply two matrices A and B with loop ordering ijk
+void multiplyMatrices_kji(int m, int k, int n, double **A, double **B, double **C) {
+    for (int l = 0; l < k; l++) {
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < m; i++) {
+                C[i][j] += A[i][l] * B[l][j];
+            }
+        }
+    }
+}
+
+void initializeResultsMatrix(int m, int n, double **matrix)  {
+      for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            matrix[i][j] = 0.0;
+        }
+    }
+}
+
+bool checkResults(int m, int n, double **matrix, double **check_matrix)  {
+      for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (matrix[i][j] != check_matrix[i][j]){
+              return false;
+            }
+        }
+    }
+    return true;
 }
 
 // Function to print a matrix
@@ -24,16 +66,24 @@ void printMatrix(int m, int n, double **matrix) {
     }
 }
 
-int main() {
+int main(int argc,char *argv[]) {
+    int m1, k1, n1;
+
+    if(argc != 4){
+      exit(1);
+    }else{
+      m1 = atoi(argv[1]);
+      k1 = atoi(argv[2]);
+      n1 = atoi(argv[3]);
+    }
     // Define the dimensions of matrices A, B, and C
-    int m1 = 1000; // number of rows of A and C for the first case
-    int k1 = 1000; // number of columns of A and rows of B
-    int n1 = 1000; // number of columns of B and C
+
 
     // Allocate memory for matrices A, B, and C
     double **A1 = (double **)malloc(m1 * sizeof(double *));
     double **B1 = (double **)malloc(k1 * sizeof(double *));
     double **C1 = (double **)malloc(m1 * sizeof(double *));
+    
     for (int i = 0; i < m1; i++) {
         A1[i] = (double *)malloc(k1 * sizeof(double));
         C1[i] = (double *)malloc(n1 * sizeof(double));
@@ -41,6 +91,8 @@ int main() {
     for (int i = 0; i < k1; i++) {
         B1[i] = (double *)malloc(n1 * sizeof(double));
     }
+    
+    initializeResultsMatrix(m1, n1, C1);
 
     // Initialize matrices A and B with random values for the first case
     for (int i = 0; i < m1; i++) {
@@ -64,6 +116,22 @@ int main() {
     end = clock();
     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
     printf("Execution time for matrix multiplication (ijk) with matrix size 1000x1000: %f seconds\n", cpu_time_used);
+
+    initializeResultsMatrix(m1, n1, C1);
+    
+    start = clock();
+    multiplyMatrices_jki(m1, k1, n1, A1, B1, C1);
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("Execution time for matrix multiplication (jki) with matrix size 1000x1000: %f seconds\n", cpu_time_used);
+    
+    initializeResultsMatrix(m1, n1, C1);
+    
+    start = clock();
+    multiplyMatrices_kji(m1, k1, n1, A1, B1, C1);
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("Execution time for matrix multiplication (kji) with matrix size 1000x1000: %f seconds\n", cpu_time_used);
 
     // Free allocated memory
     for (int i = 0; i < m1; i++) {
