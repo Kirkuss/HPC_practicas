aa44ffb19db7d91f10b49ec83d64af119395e187
Kirkuss <Enrique.Valverde1@alu.uclm.es>
2025-03-15 11:21:31 +0100

    Implementacion MPI

--- practica_I.c
+++ practica_I.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdbool.h>
-#include <string.h>
+#include <mpi.h>
+
+// Function to add two matrices A and B
+void addMatrices(int m, int n, double **A, double **B, double **C) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
+}
 
 // Function to multiply two matrices A and B with loop ordering ijk
 void multiplyMatrices_ijk(int m, int k, int n, double **A, double **B, double **C) {
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
+            C[i][j] = 0.0; 
             for (int l = 0; l < k; l++) {
                 C[i][j] += A[i][l] * B[l][j];
             }
@@ -15,48 +24,14 @@ void multiplyMatrices_ijk(int m, int k, int n, double **A, double **B, double **
     }
 }
 
-// Function to multiply two matrices A and B with loop ordering ijk
-void multiplyMatrices_jki(int m, int k, int n, double **A, double **B, double **C) {
-    for (int j = 0; j < n; j++) {
-        for (int l = 0; l < k; l++) {
-            for (int i = 0; i < m; m++) {
-                C[i][j] += A[i][l] * B[l][j];
-            }
-        }
-    }
-}
-
-// Function to multiply two matrices A and B with loop ordering ijk
-void multiplyMatrices_kji(int m, int k, int n, double **A, double **B, double **C) {
-    for (int l = 0; l < k; l++) {
-        for (int j = 0; j < n; j++) {
-            for (int i = 0; i < m; i++) {
-                C[i][j] += A[i][l] * B[l][j];
-            }
-        }
-    }
-}
-
-void initializeResultsMatrix(int m, int n, double **matrix)  {
-      for (int i = 0; i < m; i++) {
+void initializeResultsMatrix(int m, int n, double **matrix) {
+    for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             matrix[i][j] = 0.0;
         }
     }
 }
 
-bool checkResults(int m, int n, double **matrix, double **check_matrix)  {
-      for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            if (matrix[i][j] != check_matrix[i][j]){
-              return false;
-            }
-        }
-    }
-    return true;
-}
-
-// Function to print a matrix
 void printMatrix(int m, int n, double **matrix) {
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
@@ -66,35 +41,43 @@ void printMatrix(int m, int n, double **matrix) {
     }
 }
 
-int main(int argc,char *argv[]) {
+int main(int argc, char *argv[]) {
     int m1, k1, n1;
+    int rank, size;
 
-    if(argc != 4){
-      exit(1);
-    }else{
-      m1 = atoi(argv[1]);
-      k1 = atoi(argv[2]);
-      n1 = atoi(argv[3]);
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (argc != 4) {
+        if (rank == 0) {
+            printf("Usage: %s <m> <k> <n>\n", argv[0]);
+        }
+        MPI_Finalize();
+        exit(1);
+    } else {
+        m1 = atoi(argv[1]);
+        k1 = atoi(argv[2]);
+        n1 = atoi(argv[3]);
     }
-    // Define the dimensions of matrices A, B, and C
 
-
-    // Allocate memory for matrices A, B, and C
     double **A1 = (double **)malloc(m1 * sizeof(double *));
     double **B1 = (double **)malloc(k1 * sizeof(double *));
     double **C1 = (double **)malloc(m1 * sizeof(double *));
+    double **D1 = (double **)malloc(m1 * sizeof(double *));
+    double **E1 = (double **)malloc(m1 * sizeof(double *));
     
     for (int i = 0; i < m1; i++) {
         A1[i] = (double *)malloc(k1 * sizeof(double));
         C1[i] = (double *)malloc(n1 * sizeof(double));
+        D1[i] = (double *)malloc(n1 * sizeof(double));
+        E1[i] = (double *)malloc(n1 * sizeof(double));
     }
     for (int i = 0; i < k1; i++) {
         B1[i] = (double *)malloc(n1 * sizeof(double));
     }
     
-    initializeResultsMatrix(m1, n1, C1);
-
-    // Initialize matrices A and B with random values for the first case
+    srand(time(NULL) + rank); 
     for (int i = 0; i < m1; i++) {
         for (int j = 0; j < k1; j++) {
             A1[i][j] = (double)rand() / RAND_MAX;
@@ -106,37 +89,29 @@ int main(int argc,char *argv[]) {
         }
     }
 
-    // Define variables to measure execution time
-    clock_t start, end;
-    double cpu_time_used;
+    addMatrices(m1, n1, A1, B1, C1);
+    if (rank == 0) {
+        printf("Matrix C (A + B):\n");
+        printMatrix(m1, n1, C1);
+    }
 
-    // Measure execution time for matrix multiplication with loop ordering ijk for the first case
-    start = clock();
-    multiplyMatrices_ijk(m1, k1, n1, A1, B1, C1);
-    end = clock();
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    printf("Execution time for matrix multiplication (ijk) with matrix size 1000x1000: %f seconds\n", cpu_time_used);
+    addMatrices(m1, n1, C1, B1, D1);
+    if (rank == 0) {
+        printf("Matrix D (C + B):\n");
+        printMatrix(m1, n1, D1);
+    }
 
-    initializeResultsMatrix(m1, n1, C1);
-    
-    start = clock();
-    multiplyMatrices_jki(m1, k1, n1, A1, B1, C1);
-    end = clock();
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    printf("Execution time for matrix multiplication (jki) with matrix size 1000x1000: %f seconds\n", cpu_time_used);
-    
-    initializeResultsMatrix(m1, n1, C1);
-    
-    start = clock();
-    multiplyMatrices_kji(m1, k1, n1, A1, B1, C1);
-    end = clock();
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    printf("Execution time for matrix multiplication (kji) with matrix size 1000x1000: %f seconds\n", cpu_time_used);
+    multiplyMatrices_ijk(m1, k1, n1, D1, C1, E1);
+    if (rank == 0) {
+        printf("Matrix E (D * C):\n");
+        printMatrix(m1, n1, E1);
+    }
 
-    // Free allocated memory
     for (int i = 0; i < m1; i++) {
         free(A1[i]);
         free(C1[i]);
+        free(D1[i]);
+        free(E1[i]);
     }
     for (int i = 0; i < k1; i++) {
         free(B1[i]);
@@ -144,6 +119,9 @@ int main(int argc,char *argv[]) {
     free(A1);
     free(B1);
     free(C1);
+    free(D1);
+    free(E1);
 
+    MPI_Finalize();
     return 0;
 }
